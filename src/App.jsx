09ee22e47b1c9545473abaa37800b90f5aa4baab
Dede <dedenyengor09@gmail.com>
2025-11-25 16:58:44 +0000

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-function App() {
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import AddTransactionForm from "./pages/AddTransactionForm";
+import TransactionList from "./pages/TransactionList";
+import Login from "./pages/Login";
+
+
+export default function App() {
+  const router = createBrowserRouter([
+  {path: "/", element: <Dashboard/>},
+  {path: "sign-up", element: <Signup/>},
+  {path: "login", element: <Login/>},
+  {path: "add-transaction", element: <AddTransactionForm/>},
+  {path: "transaction-list", element: <TransactionList/>},
+]);
+
   return (
     <>
+    <RouterProvider router={router} />
       
     </>
-  )
-}
-
-export default App
+  )}
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,39 @@
-export default function Sidebar() {
-    return(
+export default function Signup() {
+    return (
         <>
-        <div> This is the Sidebar </div>
+            <div className="bg-blue-100 h-screen flex justify-center items-center">
+                <div className="w-[70%] m-auto h-[70vh] flex">
+                    <div className="bg-white w-1/2 h-full ">
+                        <div className="p-5 flex flex-col justify-center items-center">
+                            <p className="text-black font-bold text-2xl ">Create Account</p>
+                            <p>Join and Secure your Money</p>
+                        </div>
+
+                        <form action="" method="post" className="flex flex-col">
+                            <div className="flex flex-col pb-6 px-6">
+                                <label htmlFor="" className="font-semibold">Name</label>
+                                <input type="text" className="border rounded-lg" />
+                            </div>
+
+                            <div className="flex flex-col pb-6 px-6 ">
+                                <label htmlFor="" className="font-semibold">Email</label>
+                                <input type="email" name="email" className="border rounded-lg" />
+                            </div>
+
+                            <div className="flex flex-col pb-6 px-6">
+                                <label htmlFor="" className="font-semibold">Password</label>
+                                <input type="password" name="password" className="border rounded-lg" />
+                            </div>
+
+                            <button type="submit">Sign Up</button>
+                        </form>
+                    </div>
+                    <div className=" w-1/2 h-full">
+                      <img src="" alt="" srcset="" />
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
