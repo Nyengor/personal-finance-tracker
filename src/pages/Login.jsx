2ad4ad24aa0061c1b8a9df4b461028efd225db94
Dede <dedenyengor09@gmail.com>
2025-11-27 15:17:54 +0000

    logi ans sign up commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,42 @@
-export default function Sidebar() {
-    return(
+import Loginpic from "../images/Loginpic.jpg"
+
+
+export default function Login() {
+    return (
         <>
-        <div> This is the Sidebar </div>
+            <div className="bg-pink-200 h-screen flex justify-center items-center">
+                <div className="w-[70%] m-auto h-[70vh] flex bg-white rounded-2xl">
+                    {/* Right side */}
+                    <div className="w-1/2 h-full ">
+                        <div className="p-12 flex flex-col">
+                            <p className="text-black font-semibold text-3xl ">Welcome Back</p>
+                            <p className="text-sm">Don't have an account?<a href="sign-up" className="font-semibold">Sign Up</a></p>
+                        </div>
+
+                        <form action="" method="post" className="">
+
+                            <div className="flex flex-col pb-6 px-12 ">
+                                <label htmlFor="" className="font-semibold">Email</label>
+                                <input type="email" name="email" className="border rounded-lg" />
+                            </div>
+
+                            <div className="flex flex-col pb-6 px-12">
+                                <label htmlFor="" className="font-semibold">Password</label>
+                                <input type="password" name="password" className="border rounded-lg" />
+                            </div>
+                            <div className="flex justify-center mx-12 ">
+                            <button type="submit" className="p-2 rounded-2xl w-full text-white bg-black text-xl ">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                    
+                    {/* Left Side */}
+                    <div className=" w-1/2 h-full flex justify-center items-center">
+                        <img src={Loginpic}  alt="SLogin pic" className="w-[97%] h-[68vh]  rounded-2xl " />
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,19 +1,37 @@
+import Signuppic from "../images/signuppic.jpg"
+
+
 export default function Signup() {
     return (
         <>
-            <div className="bg-blue-100 h-screen flex justify-center items-center">
-                <div className="w-[70%] m-auto h-[70vh] flex">
-                    <div className="bg-white w-1/2 h-full ">
-                        <div className="p-5 flex flex-col justify-center items-center">
-                            <p className="text-black font-bold text-2xl ">Create Account</p>
-                            <p>Join and Secure your Money</p>
+            <div className=" bg-pink-200 h-screen flex justify-center items-center">
+                <div className="w-[70%] m-auto h-[70vh] flex bg-white rounded-2xl">
+                    {/* Right side */}
+                    <div className=" w-1/2 h-full flex justify-center items-center">
+                        <img src={Signuppic}  alt="Sign up pic" className="w-[97%] h-[68vh]  rounded-2xl " />
+                    </div>
+
+                    {/* Left side */}
+
+                    <div className="w-1/2 h-full ">
+                        <div className="p-5 flex flex-col">
+                            <p className="text-black font-semibold text-3xl ">Create Account</p>
+                            <p className="text-sm">Already have an account?<a href="login" className="font-semibold">Login</a></p>
                         </div>
 
-                        <form action="" method="post" className="flex flex-col">
-                            <div className="flex flex-col pb-6 px-6">
-                                <label htmlFor="" className="font-semibold">Name</label>
-                                <input type="text" className="border rounded-lg" />
+                        <form action="" method="post" className="">
+                            <div className="flex flex-row gap-6 pb-6 px-6 ">
+                                <div className="flex flex-col flex-1">
+                                    <label htmlFor="" className="font-semibold">First Name</label>
+                                    <input type="text" className="border rounded-lg" />
+                                </div>
+                                <div className="flex flex-col flex-1">
+                                    <label htmlFor="" className="font-semibold"> Last Name</label>
+                                    <input type="text" className="border rounded-lg" />
+
+                                </div>
                             </div>
+
 
                             <div className="flex flex-col pb-6 px-6 ">
                                 <label htmlFor="" className="font-semibold">Email</label>
@@ -24,13 +42,12 @@ export default function Signup() {
                                 <label htmlFor="" className="font-semibold">Password</label>
                                 <input type="password" name="password" className="border rounded-lg" />
                             </div>
-
-                            <button type="submit">Sign Up</button>
+                            <div className="flex justify-center mx-6 ">
+                            <button type="submit" className="p-2 rounded-2xl w-full text-white bg-black text-xl ">Sign Up</button>
+                            </div>
                         </form>
                     </div>
-                    <div className=" w-1/2 h-full">
-                      <img src="" alt="" srcset="" />
-                    </div>
+                    
                 </div>
 
             </div>
